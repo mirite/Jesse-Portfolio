3bb7bc88f180ebca328114e4b09f2c6c31ec46c6
Jesse Conner <jconner90@gmail.com>
2023-03-25 14:59:45 -0400

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import {getPosts, getPostsForStaticBuild} from "@/app/wordpress";
+import {getPost, getPosts, getPostsForStaticBuild} from "@/app/wordpress";
+import {Metadata} from "next";
 
 interface Props {
     params: {slug: string};
 }
-const Page = (props: Props) => {
+const Page = async (props: Props) => {
     const {params} = props;
+    const post = await getPost(params.slug);
     return (
         <div>
-            {params.slug}
+            <h1>{post?.title.rendered}</h1>
+            <div dangerouslySetInnerHTML={{__html: post?.content.rendered || ""}}></div>
         </div>
     );
 };
+
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+    const post = await getPost(params.slug);
+    return { title: post?.title.rendered, description: post?.excerpt.raw || ""};
+}
 
 export async function generateStaticParams() {
     return await getPostsForStaticBuild("3");
