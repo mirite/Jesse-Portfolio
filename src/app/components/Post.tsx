f387cfad63a6ff2a7ff290c866fd4b34e4518918
Jesse Conner <jconner90@gmail.com>
2023-04-23 20:03:10 -0400

    Working on text styles kinda

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Post as PostType} from "@/types";
 import {documentToReactComponents} from "@contentful/rich-text-react-renderer";
-
+import styles from './Post.module.css';
 interface Props extends PostType {}
 const Post = (post: Props) => {
     const {title, content, posted} = post;
     return (
-        <div>
+        <div className={styles.container}>
             <h1>{title}</h1>
             {documentToReactComponents(content)}
         </div>
