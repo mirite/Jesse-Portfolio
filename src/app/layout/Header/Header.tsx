0ef0388cde4a7778cacdb0bb1db6876035b707c4
Jesse Conner <me@jesseconner.ca>
2024-02-12 22:12:03 -0500

    Fixing logo

--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import React from "react";
 import Nav from "./Nav";
-
+import Image from "next/image";
+import logo from "../../svg/logo.svg";
 const Header = () => {
-  return (
-      <header className="pt-3 pb-3 mt-0 mb-0 ml-auto mr-auto flex items-baseline">
-          <div className="header-home" aria-label="Site Title">
-              <Link href="/"> Jesse Conner </Link>
-          </div>
-          <Nav/>
-</header>
-)
-    ;
+	return (
+		<header className="pt-3 pb-3 mt-0 mb-0 ml-auto mr-auto flex items-center">
+			<div className="header-home max-w-[50%]" aria-label="Site Title">
+				<Link href="/"><Image src={logo} alt={"Jesse Conner"}/></Link>
+			</div>
+			<Nav />
+		</header>
+	);
 };
 
 export default Header;
