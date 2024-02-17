43d23f208a2d880e0603a83177d8511863cc5794
Jesse Conner <me@jesseconner.ca>
2024-02-17 16:26:53 -0500

    Colour and menu fixes

--- src/app/layout/Header/NavLink.tsx
+++ src/app/layout/Header/NavLink.tsx
@@ -1,11 +1,18 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { twMerge } from "tailwind-merge";
 const NavLink = ({ path, label }: { path: string; label: string }) => {
+	const currentPath = usePathname();
 	return (
 		<li className="mr-2 prose-blue-green dark:prose-invert">
 			<Link
 				href={path}
-				className={"no-underline hover:underline transition text-2xl"}
+				className={twMerge(
+					"no-underline hover:underline transition text-2xl",
+					currentPath === path ? "underline" : "",
+				)}
 			>
 				{label}
 			</Link>
