5d03670b03e4b991ea7838dd18db955ef73cb28e
Jesse Conner <me@jesseconner.ca>
2024-02-17 19:17:36 -0500

    Fonts

--- src/app/components/Posts/Card.tsx
+++ src/app/components/Posts/Card.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Card = (props: Props) => {
 	const { title, image, link, excerpt } = props;
 	return (
-		<div className="shadow rounded-lg border transition hover:transition w-full">
+		<div className="shadow rounded-lg outline hover:bg-black  dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 transition hover:transition w-full">
 			<Link
 				className={
 					"flex flex-col w-full h-full justify-between items-start no-underline p-4  prose prose-blue-green dark:prose-invert"
@@ -19,7 +19,7 @@ const Card = (props: Props) => {
 			>
 				<h3>{title}</h3>
 				<p className={"grow"}>{excerpt}</p>
-				<div className={"flex gap-1 items-center"}>
+				<div className={"flex gap-2 items-center"}>
 					<strong className={"underline"}>Read More</strong>
 					<svg viewBox={"0 0 500 500"} width={32} height={32}>
 						<line
