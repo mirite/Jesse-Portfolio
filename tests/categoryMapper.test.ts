d8c92e98947d9fa4570928494026749207db6c57
Jesse Conner <me@jesseconner.ca>
2024-07-05 20:35:19 -0400

    Tests

---
+++ tests/categoryMapper.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import type { Category } from "../src/lib";
+import { categoryMapper } from "../src/lib";
+
+describe("categoryMapper", () => {
+	it("Should attach a slug to a category", () => {
+		const input: Category = {
+			name: "Test Category",
+			id: 1,
+		};
+		const output = categoryMapper(input);
+		expect(output.slug).toBe("test-category");
+	});
+});
---
+++ tests/stringTransform.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { createSlug, getPrettyDate } from "../src/lib/stringTransforms";
+
+describe("stringTransform", () => {
+	it("should create a url friendly slug", () => {
+		expect(createSlug("Hello World")).toBe("hello-world");
+	});
+
+	it("should make a pretty date", () => {
+		expect(getPrettyDate("2021-06-01T00:00:00.000")).toBe("6/1/2021");
+	});
+});
