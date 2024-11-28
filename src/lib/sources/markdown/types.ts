a94c61e659a3162b5f0022af173166d0bc545fdc
Jesse Conner <me@jesseconner.ca>
2024-11-28 18:25:32 -0500

    Fix typing of handler

--- src/lib/sources/markdown/types.ts
+++ src/lib/sources/markdown/types.ts
@@ -1,9 +1,12 @@
 import type { FC } from "react";
 import type { Root } from "remark-parse/lib";
 
-export type Handler<T extends Root["children"][number]["type"]> = FC<
+export type Handler<T extends NodeType> = FC<
 	Extract<Root["children"][number], { type: T }> & { key: string | number }
 >;
+
 export type Handlers = {
-	[key in Root["children"][number]["type"]]: Handler<key>;
+	[key in NodeType]: Handler<key>;
 };
+
+export type NodeType = Root["children"][number]["type"];
